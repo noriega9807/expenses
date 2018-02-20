2636369e246f3243f49bf9d38170513a53f15157
Erick Noriega <noriega0893@gmail.com>
2018-02-20 01:22:31 -0600

    Production fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const outputPath = path.join(__dirname, 'public');
+const outputPath = path.join(__dirname, 'public', 'dist');
 
 module.exports = (env) => {
 
